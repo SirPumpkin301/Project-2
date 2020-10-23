3bc4e5a2191a12e10c00f946c19e40c0cc8e6452
Cynthia Mojica <cmojica1231@gmail.com>
2020-10-22 18:20:49 -0700

    modify server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "test") {
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen((process.env.PORT || 3000), function(){
+  app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 });
